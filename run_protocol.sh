7300dccff5f90b05a9ec141d4ebabe3e52b2345d
liork <liork.cryptobiu@gmail.com>
2017-01-29 12:16:36 +0200

    fix number of parameters in script

--- run_protocol.sh
+++ run_protocol.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 for i in `seq $1 1 $2`;
 do	
-	./Secret_Sharing $i $2 $3 $4 output.txt $5 tcp://192.168.0.12:1883 GF2m &
+	./Secret_Sharing $i $3 $4 output.txt $5 tcp://192.168.0.12:1883 GF2m &
 	echo "Running $i..."
 done
