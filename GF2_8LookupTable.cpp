84845922bbac582e688f12450ff152621b29db28
levymeit <levymeit@gmail.com>
2017-05-29 12:53:37 +0300

    adding parallel execution by adding groupId

---
+++ GF2_8LookupTable.cpp
@@ -0,0 +1,130 @@
+//
+// Created by meital on 17/05/17.
+//
+
+#include "GF2_8LookupTable.h"
+
+
+
+
+using namespace std;
+using namespace NTL;
+
+byte GF2_8LookupTable::multTable[256][256] = {};
+
+void GF2_8LookupTable::initTable(){
+
+
+    GF2X irreduciblePolynomial = BuildSparseIrred_GF2X(8);
+    GF2E::init(irreduciblePolynomial);
+
+
+    byte elem1[1];
+    byte elem2[1];
+    byte resultInBytes[1];
+
+
+    //first create a GF2X
+    GF2X polynomialElement1, polynomialElement2;
+
+
+    //use the NTL lib to create the lookup table
+    for(unsigned int i=0; i<256; i++)
+    {
+        elem1[0] = (byte)i;
+        //translate the bytes into a GF2X element
+        GF2XFromBytes(polynomialElement1, elem1, 1);
+        for(unsigned int j=0; j<256; j++){
+
+
+            elem2[0] = (byte)j;
+            GF2XFromBytes(polynomialElement2, elem2, 1);
+
+
+            auto result = to_GF2E(polynomialElement1) * to_GF2E(polynomialElement2);
+
+            BytesFromGF2X(resultInBytes,rep(result),1);
+
+            GF2_8LookupTable::multTable[i][j] = resultInBytes[0];
+        }
+    }
+
+
+}
+
+
+GF2_8LookupTable& GF2_8LookupTable::operator=(const GF2_8LookupTable& other) // copy assignment
+{
+    if (this != &other) { // self-assignment check expected
+        elem = other.elem;
+    }
+    return *this;
+}
+
+
+
+
+GF2_8LookupTable GF2_8LookupTable::operator*(const GF2_8LookupTable& f2)
+{
+
+    GF2_8LookupTable answer;
+
+    auto elemleft = (unsigned int)f2.elem;
+    auto elemright = (unsigned int)elem;
+
+    if(elemleft>255 || elemright>255) {
+        cout << "wrong values: elemleft = " << elemleft << "elemright = " << elemright;
+        exit(0);
+    }
+
+    answer.elem = multTable[elemleft][elemright];
+
+    return answer;
+
+}
+
+GF2_8LookupTable& GF2_8LookupTable::operator*=(const GF2_8LookupTable& f2){
+
+    auto elemleft = (unsigned int)f2.elem;
+    auto elemright = (unsigned int)elem;
+
+    if(elemleft>255 || elemright>255) {
+        cout << "wrong values: elemleft = " << elemleft << "elemright = " << elemright;
+        exit(0);
+    }
+
+    elem = multTable[elemleft][elemright];
+
+}
+
+GF2_8LookupTable GF2_8LookupTable::operator/(const GF2_8LookupTable& f2) {
+
+    GF2_8LookupTable answer;
+
+    byte elem1[1];
+    byte elem2[1];
+    byte resultInBytes[1];
+
+
+    //first create a GF2X
+    GF2X polynomialElement1, polynomialElement2;
+
+
+    elem1[0] = (byte) f2.elem;
+    //translate the bytes into a GF2X element
+    GF2XFromBytes(polynomialElement1, elem1, 1);
+
+
+    elem2[0] = (byte) elem;
+    GF2XFromBytes(polynomialElement2, elem2, 1);
+
+
+    auto result = to_GF2E(polynomialElement1) / to_GF2E(polynomialElement2);
+
+    BytesFromGF2X(resultInBytes, rep(result), 1);
+
+    answer.elem = resultInBytes[0];
+
+    return answer;
+
+}
---
+++ GF2_8LookupTable.h
@@ -0,0 +1,49 @@
+//
+// Created by meital on 17/05/17.
+//
+
+#ifndef SECRET_SHARING_GF2_8LOOKUPTABLE_H
+#define SECRET_SHARING_GF2_8LOOKUPTABLE_H
+
+#include <NTL/GF2E.h>
+#include <NTL/GF2X.h>
+#include <NTL/ZZ_p.h>
+#include <NTL/GF2XFactoring.h>
+#include <libscapi/include/infra/Common.hpp>
+
+
+
+
+class GF2_8LookupTable {
+
+
+public:
+    byte elem;
+
+
+
+public:
+
+    static byte multTable[256][256];
+    static void initTable();
+    GF2_8LookupTable(){elem = 0;};
+    GF2_8LookupTable(unsigned int elem): elem(elem){};
+
+    GF2_8LookupTable& operator=(const GF2_8LookupTable& other);
+    inline bool operator!=(const GF2_8LookupTable& other){ return !(other.elem == elem); };
+
+    inline GF2_8LookupTable operator+(const GF2_8LookupTable& f2){return GF2_8LookupTable(f2.elem ^ elem);};
+    GF2_8LookupTable operator-(const GF2_8LookupTable& f2){return GF2_8LookupTable(f2.elem ^ elem);};
+    GF2_8LookupTable operator/(const GF2_8LookupTable& f2);
+    GF2_8LookupTable operator*(const GF2_8LookupTable& f2);
+
+    inline GF2_8LookupTable& operator+=(const GF2_8LookupTable& f2){ elem = (f2.elem ^ elem) ; return *this;};
+    GF2_8LookupTable& operator*=(const GF2_8LookupTable& f2);
+
+
+
+};
+
+inline ::ostream& operator<<(::ostream& s, const GF2_8LookupTable& a){ return s << a.elem; };
+
+#endif //SECRET_SHARING_GF2_8LOOKUPTABLE_H
