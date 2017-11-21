c2615f14220428910b2ae5b699faf8d95ac3ec45
liorbiu <liork.cryptobiu@gmail.com>
2017-11-21 10:12:29 +0200

    Add parameter for parties file

--- run_protocol.sh
+++ run_protocol.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 for i in `seq $1 1 $2`;
 do
-	./Secret_Sharing -partyID $i -partiesNumber $3 -inputFile $4 -outputFile output.txt -circuitFile $5 -partiesFile Parties.txt -fieldType $6 -internalIterationsNumber $7 &
+	./Secret_Sharing -partyID ${i} -partiesNumber ${3} -inputFile ${4} -outputFile output.txt -circuitFile ${5} -partiesFile ${6} -fieldType ${7} -internalIterationsNumber ${8} &
 	echo "Running $i..."
 done
